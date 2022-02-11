ac20353ce51f5a173bbda1802681806a586f25d8
michaeljayl <mjl@nova.edu>
2022-02-11 04:46:47 -0500

    Add files via upload

--- graphicsN/js/mandelbrot3D_EN.js
+++ graphicsN/js/mandelbrot3D_EN.js
@@ -130,7 +130,7 @@ function invertMandelbrotLandscape() {
     pos.needsUpdate = true;
 }
 
-const log2 = Math.log(2.0);
+
 
 function mandelbrotFrac(cr, ci, limit) {
     let zr = 0.0;
@@ -148,7 +148,7 @@ function mandelbrotFrac(cr, ci, limit) {
     }
     if (i < limit) {
         let modulus = Math.sqrt(zr2 + zi2);
-        let f = Math.log(Math.log(modulus)) / log2;
+        let f = Math.log2(Math.log2(modulus)) / 4;
         return (i+1) - f;
     } else {
         return limit;
